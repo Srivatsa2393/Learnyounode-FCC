65b38dc9ce254ccbf3a7e95f105c30acbaa74341
Srivatsa S <srivatsashankaran@gmail.com>
2017-06-29 20:00:31 +0530

    My first syncronous i/o from reading the file system

---
+++ 03_my_first_io.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const buffer = fs.readFileSync(process.argv[2]);
+
+const result = buffer.toString().split('\n').length - 1;
+
+console.log(result);
+//console.log(buffer.toString());
