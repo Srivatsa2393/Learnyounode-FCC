5ba986783db827caa4fc54fbe460ebd1eaaac6f3
Srivatsa S <srivatsashankaran@gmail.com>
2017-09-07 23:07:09 +0530

    http collect using bufferlist

---
+++ 08_http_collect.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+const url = process.argv[2];
+
+/* http.get(url, response => {
+
+    let result = '';
+    response.setEncoding('utf8');
+
+
+    response.on('error', (err) => {
+        console.error(err);
+    });
+
+
+    response.on('data', (data) => {
+        result += data;
+    });
+
+    response.on('end', () => {
+        console.log(result.length);
+        console.log(result);
+    });
+
+}); */
+
+//Using a 3rd party service
+
+const bl = require('bl');
+
+http.get(url, response => {
+    response.pipe(bl((err, data) => {
+        if (err) return console.error(err);
+
+        data = data.toString();
+        console.log(data.length);
+        console.log(data);
+    }));
+});
